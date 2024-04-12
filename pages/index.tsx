4feb00a539fa5941cb0a4ea51d8f1ae562caf0b2
Bryan De Jesús Rosa Tavarez <20212239@itla.edu.do>
2024-04-12 10:01:38 -0400

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,9 @@ import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 import LoadingDots from '@/components/ui/LoadingDots';
 import { Document } from 'langchain/document';
+import { FileInput, Label } from 'flowbite-react';
+import { Button, Modal } from 'flowbite-react';
+import { HiOutlineDocumentText } from 'react-icons/hi2';
 import {
   Accordion,
   AccordionContent,
@@ -25,7 +28,7 @@ export default function Home() {
   }>({
     messages: [
       {
-        message: 'Hi, what would you like to learn about this document?',
+        message: 'Hola, ¿qué te gustaría aprender sobre este documento?',
         type: 'apiMessage',
       },
     ],
@@ -48,7 +51,7 @@ export default function Home() {
     setError(null);
 
     if (!query) {
-      alert('Please input a question');
+      alert('Por favor ingresa una pregunta.');
       return;
     }
 
@@ -106,7 +109,9 @@ export default function Home() {
       messageListRef.current?.scrollTo(0, messageListRef.current.scrollHeight);
     } catch (error) {
       setLoading(false);
-      setError('An error occurred while fetching the data. Please try again.');
+      setError(
+        'Ocurrio un error cuando se obtenía la data . Por favor trata de intentarlo nuevamente.',
+      );
       console.log('error', error);
     }
   }
@@ -120,12 +125,30 @@ export default function Home() {
     }
   };
 
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      setSelectedFile(file);
+    }
+  };
+
+  // Aquí puedes agregar la lógica para enviar el archivo seleccionado al servidor cuando sea necesario
+  const handleUpload = () => {
+    if (selectedFile) {
+      // Aquí puedes enviar `selectedFile` al servidor (por ejemplo, usando fetch o una librería como axios)
+      console.log('Archivo seleccionado:', selectedFile);
+    }
+  };
+
+  const [openModal, setOpenModal] = useState(false);
   return (
     <>
       <Layout>
         <div className="mx-auto flex flex-col gap-4">
           <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
-            Chat With Your Docs
+            Chatea con tus documentos
           </h1>
           <main className={styles.main}>
             <div className={styles.cloud}>
@@ -188,14 +211,14 @@ export default function Home() {
                               <div key={`messageSourceDocs-${index}`}>
                                 <AccordionItem value={`item-${index}`}>
                                   <AccordionTrigger>
-                                    <h3>Source {index + 1}</h3>
+                                    <h3>Fuente {index + 1}</h3>
                                   </AccordionTrigger>
                                   <AccordionContent>
                                     <ReactMarkdown linkTarget="_blank">
                                       {doc.pageContent}
                                     </ReactMarkdown>
                                     <p className="mt-2">
-                                      <b>Source:</b> {doc.metadata.source}
+                                      <b>Fuente:</b> {doc.metadata.source}
                                     </p>
                                   </AccordionContent>
                                 </AccordionItem>
@@ -223,8 +246,8 @@ export default function Home() {
                     name="userInput"
                     placeholder={
                       loading
-                        ? 'Waiting for response...'
-                        : 'What is this legal case about?'
+                        ? 'Esperando una respuesta...'
+                        : '¿De qué se trata este documento?'
                     }
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
@@ -235,21 +258,72 @@ export default function Home() {
                     disabled={loading}
                     className={styles.generatebutton}
                   >
-                    {loading ? (
-                      <div className={styles.loadingwheel}>
-                        <LoadingDots color="#000" />
-                      </div>
-                    ) : (
-                      // Send icon SVG in input field
-                      <svg
-                        viewBox="0 0 20 20"
-                        className={styles.svgicon}
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
-                      </svg>
-                    )}
+                    <svg
+                      viewBox="0 0 20 20"
+                      className={styles.svgicon}
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+                    </svg>
                   </button>
+                  <Button
+                    className={styles.generatebutton2}
+                    onClick={() => setOpenModal(true)}
+                  >
+                    <HiOutlineDocumentText className="text-lg -mt-1 " />
+                  </Button>
+                  <Modal show={openModal} onClose={() => setOpenModal(false)}>
+                    <Modal.Header>Subir Documento</Modal.Header>
+                    <Modal.Body>
+                      <div className="space-y-6">
+                        <div className="flex w-full items-center justify-center">
+                          <Label
+                            htmlFor="dropzone-file"
+                            className="flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                          >
+                            <div className="flex flex-col items-center justify-center pb-6 pt-5">
+                              <svg
+                                className="mb-4 h-8 w-8 text-gray-500 dark:text-gray-400"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 20 16"
+                              >
+                                <path
+                                  stroke="currentColor"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeWidth="2"
+                                  d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                                />
+                              </svg>
+                              <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                                <span className="font-semibold">
+                                  Click para subir un documento
+                                </span>{' '}
+                                o arrastrar y suelta aquí
+                              </p>
+                              <p className="text-xs text-gray-500 dark:text-gray-400">
+                                PDF, MAX. 10 MB
+                              </p>
+                            </div>
+                            <FileInput
+                              id="dropzone-file"
+                              className="hidden"
+                              onChange={handleFileChange}
+                              accept=".pdf" // Acepta solo archivos PDF
+                            />
+                          </Label>
+                        </div>
+                      </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button onClick={handleUpload}>Subir</Button>
+                      <Button color="gray" onClick={() => setOpenModal(false)}>
+                        Cancelar
+                      </Button>
+                    </Modal.Footer>
+                  </Modal>
                 </form>
               </div>
             </div>
@@ -261,8 +335,9 @@ export default function Home() {
           </main>
         </div>
         <footer className="m-auto p-4">
-          <a href="https://twitter.com/mayowaoshin">
-            Powered by LangChainAI. Demo built by Mayo (Twitter: @mayowaoshin).
+          <a href="https://twitter.com/">
+            Desarrollado por Alexander Gemoso. Demostración creada por
+            Eduminicana .
           </a>
         </footer>
       </Layout>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const flowbite = require('flowbite-react/tailwind');
 module.exports = {
   content: [
+    flowbite.content(),
     './app/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  plugins: [
+    // ...
+    flowbite.plugin(),
   ],
   theme: {
     extend: {},
